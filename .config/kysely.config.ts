11543a9e6bcaa05ca488b3f337958719f9e31297
bernardorodriguesss <bernardo029.rodrigues@gmail.com>
2026-01-08 12:58:04 -0300

    chore(db): initial migrations setup

---
+++ .config/kysely.config.ts
@@ -0,0 +1,17 @@
+import { PostgresDialect } from 'kysely';
+import { defineConfig } from 'kysely-ctl';
+import { Pool } from 'pg';
+
+export default defineConfig({
+	dialect: new PostgresDialect({
+		pool: new Pool({ connectionString: process.env.DATABASE_URL }),
+	}),
+	migrations: {
+		migrationFolder: 'migrations',
+		migrationTableName: 'migrations_table',
+		migrationLockTableName: 'migrations_lock',
+	},
+	seeds: {
+		seedFolder: 'seeds',
+	},
+});
---
+++ .config/migrations/001_users_table.ts
@@ -0,0 +1,23 @@
+import { sql, type Kysely } from 'kysely';
+
+export async function up(db: Kysely<any>): Promise<void> {
+	await sql`
+		CREATE TYPE user_role AS ENUM ('ADMIN', 'USER');
+	`.execute(db);
+
+	await sql`
+		CREATE TABLE users (
+			id UUID PRIMARY KEY,
+			name VARCHAR NOT NULL,
+			email VARCHAR NOT NULL UNIQUE,
+			password_hash VARCHAR NOT NULL,
+			role user_role NOT NULL,
+			created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+		);
+	`.execute(db);
+}
+
+export async function down(db: Kysely<any>): Promise<void> {
+	await sql`DROP TABLE IF EXISTS users`.execute(db);
+	await sql`DROP TYPE IF EXISTS user_role`.execute(db);
+}
